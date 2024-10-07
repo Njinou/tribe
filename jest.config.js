1cf8e5cfea644815a33f4040c8101016a0364155
Pascal Nitcheu <nitcheupascal@gmail.com>
2024-10-07 04:38:35 +0100

    configuring test and babel and adding tsx extension to raise the extensions allowed and babel for any transformation necessary

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,10 @@ module.exports = {
     preset: 'ts-jest',
     testEnvironment: 'node',
     transform: {
-      '^.+\\.ts$': 'ts-jest'
-    }
+        '^.+\\.jsx?$': 'babel-jest', // Transforms JS and JSX using Babel
+        '^.+\\.ts$': 'ts-jest',      // Transforms TS using ts-jest
+        '^.+\\.tsx?$': 'ts-jest'     // Transforms TSX using ts-jest
+      },       // or 'jsdom' based on your project
+      moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx'], // File extensions to test
   };
   
